4e4d4814e59065d347c9fa4b80b69b67461f8732
vanphuoc369 <vanphuoc369@gmail.com>
2018-01-05 13:02:13 +0700

    Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get  "/signup", to: "users#new"
+  get "/signup", to: "users#new"
   resources :users, except: %i(edit, update, destroy)
   root "static_pages#home"
   resources :books
