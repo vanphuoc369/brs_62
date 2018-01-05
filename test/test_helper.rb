429a318866a63367749978cb8a357f1b06c0c823
vanphuoc369 <vanphuoc369@gmail.com>
2018-01-05 13:02:13 +0700

    Static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,5 @@
-ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/reporters"
-Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
