ed8254e20f984e3f549d437cfa5dc3aabb7babe7
nguyenquangdung14cntt <nguyenquangdung5560@gmail.com>
2018-01-08 12:17:22 +0700

    Sign In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".please_log_in"
+    redirect_to login_url
+  end
 end
