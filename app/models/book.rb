fac18a98f593e414316feee75d3f50470c62835c
vanphuoc369 <vanphuoc369@gmail.com>
2018-01-09 17:30:32 +0700

    Full search

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,4 +7,24 @@ class Book < ApplicationRecord
   has_many :categories, through: :book_categories
 
   scope :alpha, ->{order title: :desc}
+  scope :search_for_title, -> text do
+    where("title LIKE ?", "%#{text}%") if text.present?
+  end
+  scope :search_for_author, -> text do
+    where("author LIKE ?", "%#{text}%") if text.present?
+  end
+  scope :books_rating,-> star do
+    joins(:reviews).group(:book_id).having("avg(rate) between #{star}-0.5 and #{star}+0.5")
+  end
+  scope :search_for_favorite, -> id do
+    joins(:user_books).where("user_books.user_id =  #{id} and user_books.is_favorite = 1") if id.present?
+  end
+
+  def self.search text_search, search_for
+    if search_for ==I18n.t("nav_bar.submit_author")
+      Book.search_for_author text_search
+    else
+      Book.search_for_title text_search
+    end
+  end
 end
